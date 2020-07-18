40f9f9dc6fb7a703a3485867ff5b3bed3160f31e
rimever <mot_siva_ordin@hotmail.com>
2020-07-18 16:34:53 +0900

    本を見ながら書いたコードをコミット。

---
+++ Hello/index.js
@@ -0,0 +1,22 @@
+const {app,BrowserWindow} = require('electron');
+
+let win;
+
+function initWindow() {
+    win = new BrowserWindow({width:600,height:400});
+    win.loadURL(`file://${__dirname}/index.html`);
+    win.on('closed',() => {win = null;});
+}
+app.on('ready',initWindow);
+
+app.on('window-all-closed',() => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('active', () => {
+    if (win === null) {
+        initWindow();
+    }
+});
